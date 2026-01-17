6a27df4417a346be77005585e3aa4e3b665f15e5
Lior Goldberg <lior@starkware.co>
2026-01-17 18:44:49 +0200

    Remove kbob from amounts.lean.

--- lean/privacy/tracking/tracking.lean
+++ lean/privacy/tracking/tracking.lean
@@ -4,9 +4,7 @@ import privacy.tracking.incoming_notes
 theorem incoming_eq_outgoing
   {crypto: Crypto} (stxs: SuccessfulTransactions crypto) (addr: ℕ) (token: ℕ) : false := by
   have := outgoing_notes stxs addr token
-  conv at this =>
-    rhs; enter [1, 1, tx]
-    rw [tx.h_balance]
+  conv at this => rhs; enter [1, 1, tx]; rw [tx.h_balance]
   rw [←incoming_notes] at this
 
   sorry
