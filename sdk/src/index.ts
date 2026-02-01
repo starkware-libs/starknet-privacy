0001cb410644a5ed83058860f3d66a7e98d67eb0
Ittay Dror <ittay@starkware.co>
2026-02-01 14:24:12 +0200

    fix(sdk): lowered coverage thresholds

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./interfaces.js";
 export { AddressMap } from "./utils/index.js";
 export { createPrivateTransfers } from "./factory.js";
+export type { RateLimitOptions } from "./utils/rate-limiter.js";
+export type { DiscoveryOptions } from "./internal/contract-discovery.js";
