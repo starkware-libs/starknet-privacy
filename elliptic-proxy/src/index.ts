88be92fc0087e8f6701cc768af14846feddf0337
Ittay Dror <107039793+ittaysw@users.noreply.github.com>
2026-04-16 09:28:23 +0300

    feat(elliptic-proxy): screening proxy with hardcoded blocked response (#677)
    <!-- Reviewable:start -->
    This change is [<img src="https://reviewable.io/review_button.svg" height="34" align="absmiddle" alt="Reviewable"/>](https://reviewable.io/reviews/starkware-libs/starknet-privacy/677)
    <!-- Reviewable:end -->

---
+++ elliptic-proxy/src/index.ts
@@ -0,0 +1,22 @@
+// src/index.ts
+import * as ff from "@google-cloud/functions-framework";
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
+import { ConfigLoader } from "./config.js";
+import { forwardToElliptic } from "./elliptic.js";
+import { createHandler } from "./handler.js";
+
+function createSecretFetcher(): () => Promise<string> {
+  const client = new SecretManagerServiceClient();
+  const secretName = process.env.PROXY_CONFIG;
+  return async () => {
+    if (!secretName) throw new Error("PROXY_CONFIG env var not set");
+    const [version] = await client.accessSecretVersion({
+      name: secretName,
+    });
+    return version.payload?.data?.toString() ?? "";
+  };
+}
+
+const configLoader = new ConfigLoader(createSecretFetcher());
+
+ff.http("ellipticProxy", createHandler(configLoader, forwardToElliptic));
